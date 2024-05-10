49b8938b0295d8ded95419db8ef769eeda3db107
Fares Karim <fareskarimelkholy@gmail.com>
2024-05-10 16:35:52 +0300

    Added course repo functionality

--- server/learning/src/main/java/com/online/api/CourseApi.java
+++ server/learning/src/main/java/com/online/api/CourseApi.java
@@ -5,12 +5,16 @@ import com.online.model.Course;
 
 import jakarta.ejb.EJB;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 import java.util.List;
 
@@ -23,12 +27,6 @@ public class CourseApi {
 
   @EJB
   CourseRepository courseRepo;
-
-  @GET
-  @Path("/test")
-  public String test() {
-    return "Hello World!";
-  }
 
   @POST
   @Path("/create")
@@ -51,14 +49,63 @@ public class CourseApi {
   }
 
   @GET
-  @Path("/course")
-  public Response getCourseByTerm(String searchTerm) {
+  @Path("/search/{searchTerm}")
+  public Response getCourseByTerm(@PathParam("searchTerm") String searchTerm) {
     List<Course> courses = courseRepo.findCourses(searchTerm);
     if (courses == null) {
       return Response.serverError().build();
     }
+    if (courses.isEmpty()) {
+      return Response.status(Status.NOT_FOUND).build();
+    }
     return Response.ok(courses).build();
   }
 
-  
+  @PUT
+  @Path("/update/{id}")
+  public Response updateCourse(@PathParam("id") long id, Course course) {
+    String response;
+    try {
+      response = courseRepo.updateCourse(id, course);
+    } catch (Exception e) {
+      return Response.serverError().build();
+    }
+    return Response.ok(response).build();
+  }
+
+  @PUT
+  @Path("/accept/{id}")
+  public Response acceptCourseById(@PathParam("id") long id) {
+    String response;
+    try {
+      response = courseRepo.acceptCourse(id);
+    } catch (Exception e) {
+      return Response.serverError().build();
+    }
+    return Response.ok(response).build();
+  }
+
+  @PUT
+  @Path("/reject/{id}")
+  public Response rejectCourseById(@PathParam("id") long id) {
+    String response;
+    try {
+      response = courseRepo.rejectCourse(id);
+    } catch (Exception e) {
+      return Response.serverError().build();
+    }
+    return Response.ok(response).build();
+  }
+
+  @DELETE
+  @Path("/remove/{id}")
+  public Response removeCourseById(@PathParam("id") long id) {
+    String response;
+    try {
+      response = courseRepo.removeCourse(id);
+    } catch (Exception e) {
+      return Response.serverError().build();
+    }
+    return Response.ok(response).build();
+  }
 }
--- server/learning/src/main/java/com/online/controllers/CourseRepository.java
+++ server/learning/src/main/java/com/online/controllers/CourseRepository.java
@@ -24,7 +24,7 @@ public class CourseRepository {
       obj.setCategory(course.getCategory());
       obj.setContent(course.getContent());
       obj.setRating(0.0);
-      obj.setStatus("pending");
+      obj.setStatus("PENDING");
       em.persist(obj);
       return obj;
     } catch (Exception e) {
@@ -32,38 +32,71 @@ public class CourseRepository {
     }
   }
 
-  public String acceptCourse(int courseId) {
-    try {
-      Course course = em.find(Course.class, courseId);
-      if (course != null) {
-        em.detach(course);
-        course.setStatus("ACCEPTED");
-        em.getTransaction().begin();
-        em.merge(course);
-        em.getTransaction().commit();
-        return "Course accepted successfuly!";
-      }
-      return "Course not found!";
-    } catch (Exception e) {
-      return null;
+  public Course findCourseById(long id) {
+    Course course = em.find(Course.class, id);
+    if (course != null) {
+      return course;
     }
+    return null;
   }
 
-  public String rejectCourse(int courseId) {
-    try {
-      Course course = em.find(Course.class, courseId);
-      if (course != null) {
-        em.detach(course);
-        course.setStatus("REJECTED");
-        em.getTransaction().begin();
+  public String acceptCourse(long id) throws Exception {
+    Course course = em.find(Course.class, id);
+    if (course != null) {
+      try {
+        course.setStatus("ACCEPTED");
         em.merge(course);
-        em.getTransaction().commit();
-        return "Course rejected successfuly!";
-      }
-      return "Course not found!";
-    } catch (Exception e) {
-      return null;
+        return "Course accepted successfuly!";
+      } catch (Exception e) {
+        throw e;
+      } 
     }
+    return "Course not found!";
+  }
+
+  public String rejectCourse(long id) throws Exception {
+    Course course = em.find(Course.class, id);
+    if (course != null) {
+      try {
+        course.setStatus("REJECTED");
+        em.merge(course);
+        return "Course rejected successfuly!";
+      } catch (Exception e) {
+        throw e;
+      }
+    }
+    return "Course not found!";
+  }
+
+  public String removeCourse(long id) throws Exception {
+    Course course = em.find(Course.class, id);
+    if (course != null) {
+      try {
+        em.remove(course);
+        return "Course removed successfully!";
+      } catch (Exception e) {
+        throw e;
+      }
+    }
+    return "Course not found!";
+  }
+
+  public String updateCourse(long id, Course course) throws Exception {
+    Course obj = em.find(Course.class, id);
+    if (obj != null) {
+      try {
+        obj.setName(course.getName());
+        obj.setDuration(course.getDuration());
+        obj.setCapacity(course.getCapacity());
+        obj.setCategory(course.getCategory());
+        obj.setContent(course.getContent());
+        em.merge(obj);
+        return "Course updated successfuly!";
+      } catch (Exception e) {
+        throw e;
+      }
+    }
+    return "Course not found!";
   }
 
   public List<Course> listAllCourses() {
@@ -77,7 +110,7 @@ public class CourseRepository {
 
   public List<Course> listAllValidCourses() {
     try {
-      TypedQuery<Course> courses = em.createQuery("SELECT c FROM Course c WHERE c.status = ACCEPTED", Course.class);
+      TypedQuery<Course> courses = em.createQuery("SELECT c FROM Course c WHERE c.status = 'ACCEPTED'", Course.class);
       return courses.getResultList();
     } catch (Exception e) {
       return null;
@@ -87,10 +120,13 @@ public class CourseRepository {
   public List<Course> findCourses(String searchTerm) {
     try {
       TypedQuery<Course> query = em
-          .createQuery("SELECT c FROM Course c WHERE c.name = :searchTerm OR c.category = :searchTerm", Course.class);
+          .createQuery(
+              "SELECT c FROM Course c WHERE (c.name = :searchTerm OR c.category = :searchTerm) AND c.status = 'ACCEPTED'",
+              Course.class);
       query.setParameter("searchTerm", searchTerm);
       return query.getResultList();
     } catch (Exception e) {
+      e.printStackTrace();
       return null;
     }
   }
