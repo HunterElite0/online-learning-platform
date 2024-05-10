4f61eb7997ae8e2964c9a1399d4b1d8765253ffa
Fares Karim <fareskarimelkholy@gmail.com>
2024-05-10 23:40:21 +0300

    Updated dockerfile and start script

--- server/start.sh
+++ server/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Build locally (development)
+mvn clean package -f "learning/pom.xml"
+
 docker build -t online-learning .
 docker run --rm  -p 8080:8080 -p 9990:9990 --name learning online-learning
